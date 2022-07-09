3e62c78a8744c58d0af1907c6de0bafea2207002
Alexey <aleks-samos@yandex.ru>
2022-07-09 20:54:52 +0300

    fix players pairs chain generation

--- services/game.py
+++ services/game.py
@@ -1,9 +1,20 @@
-from typing import Any, List, Tuple
+from typing import Any, List, Optional, Tuple
 
 
-def make_pairs_of_players(players: list) -> List[Tuple[Any, Any]]:
+def make_pairs_of_players(
+    players: list,
+    back_order:Optional[bool] = False
+) -> List[Tuple[Any, Any]]:
     "Генерирует пары игроков"
-    from itertools import combinations
-
-    pairs = list(combinations(players, 2)) + list(combinations(players[::-1], 2))
+    if len(players) < 2: return [] # type: ignore
+    pairs:list = []
+    for i in range(len(players)):
+        pair = None
+        try:
+            pair = [players[i], players[i+1]]
+            if back_order:
+                pair = pair[::-1]
+            pairs.append((pair[0], pair[1]))
+        except IndexError:
+            pass
     return pairs
--- tests/test_make_pairs_of_players.py
+++ tests/test_make_pairs_of_players.py
@@ -1,11 +1,28 @@
 import unittest
-from services.game import make_pairs_of_players # type: ignore
+from services.game import make_pairs_of_players as make # type: ignore
 
 class TestServicesGameMakePairs(unittest.TestCase):
-    def test_make_pairs_of_players(self):
-        players = [1,2,3]
-        expected_pairs = [(1, 2), (1, 3), (2, 3), (3, 2), (3, 1), (2, 1)]
-        pairs = make_pairs_of_players(players)
+    def setUp(self):
+        self.        players = list("qwerty")
+
+    def test_forward(self):
+        expected_pairs = [('q', 'w'), ('w', 'e'), ('e', 'r'), ('r', 't'), ('t', 'y')]
+        pairs = make(self.players, False)
+        self.assertCountEqual(expected_pairs, pairs)
+        self.assertListEqual(expected_pairs, pairs)
+
+    def test_backward(self):
+        expected_pairs = [('w', 'q'), ('e', 'w'), ('r', 'e'), ('t', 'r'), ('y', 't')]
+        pairs = make(self.players, True)
+        self.assertCountEqual(expected_pairs, pairs)
+        self.assertListEqual(expected_pairs, pairs)
+
+    def test_together(self):
+        expected_pairs = [
+                ('q', 'w'), ('w', 'e'), ('e', 'r'), ('r', 't'), ('t', 'y'),
+                ('w', 'q'), ('e', 'w'), ('r', 'e'), ('t', 'r'), ('y', 't')
+        ]
+        pairs = make(self.players, False) + make(self.players, True)
         self.assertCountEqual(expected_pairs, pairs)
         self.assertListEqual(expected_pairs, pairs)
 
