1749501d020ac37de9cb70107f2f7943c0e27d36
Alexey <aleks-samos@yandex.ru>
2022-07-20 18:13:18 +0300

    add service worker for cache

---
+++ static/sw.js
@@ -0,0 +1,46 @@
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open('v1').then(function(cache) {
+      return cache.addAll([
+        '/tod/index.html', '/tod/script.js', '/tod/sw.js'
+      ]);
+    })
+  );
+});
+self.addEventListener('fetch', function(event) {
+  event.respondWith(caches.match(event.request).then(function(response) {
+    // caches.match() always resolves
+    // but in case of success response will have value
+    if (response !== undefined) {
+      return response;
+    } else {
+      return fetch(event.request).then(function (response) {
+        // response may be used only once
+        // we need to save clone to put one copy in cache
+        // and serve second one
+        let responseClone = response.clone();
+        caches.open('v1').then(function (cache) {
+          cache.put(event.request, responseClone);
+        });
+        return response;
+      }).catch(function () {
+        return caches.match('/tod/index.html');
+      });
+    }
+  }));
+});
+// register service worker
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('/tod/sw.js', { scope: '/tod/' }).then(function(reg) {
+    if(reg.installing) {
+      console.log('Service worker installing');
+    } else if(reg.waiting) {
+      console.log('Service worker installed');
+    } else if(reg.active) {
+      console.log('Service worker active');
+    }
+  }).catch(function(error) {
+    // registration failed
+    console.log('Registration failed with ' + error);
+  });
+}
